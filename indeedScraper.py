5993924c60e54bc02f24899f4fe643da700fe51f
LewisShutler <lewisshutler@hotmail.co.uk>
2021-01-23 21:04:11 +0000

    decluttered the code

--- indeedScraper.py
+++ indeedScraper.py
@@ -18,47 +18,33 @@ def find_jobs():
         # load indeed jobs
         page = requests.get(url)
         soup = BeautifulSoup(page.content, "html.parser")
-        job_soup = soup.find(id="resultsCol")
+        
         
         #Extract job information
-        job_elems = job_soup.find_all('div', class_='jobsearch-SerpJobCard')
-        
-        cols = ['titles', 'companies', 'links', 'date_listed']
+        job_elems = soup.find_all('div', class_='jobsearch-SerpJobCard')
 
-        cols.append('titles')
+        
         for job_elem in job_elems:
             title_elem = job_elem.find('h2', class_='title')
-            title = title_elem.text.strip()
-            title = title.strip('new')
-            titles.append(title)
-        extracted_info.append(titles)                    
+            title = title_elem.text.strip().strip('\nnew')
+            titles.append(title) 
 
-        for job_elem in job_elems:
             company_elem = job_elem.find('span', class_='company')
             company = company_elem.text.strip()
             companies.append(company)
-        extracted_info.append(companies)
-
-        for job_elem in job_elems:
+  
             link = job_elem.find('a')['href']
             link = 'www.Indeed.co.uk' + link
             links.append(link)
-        extracted_info.append(links)
 
-        for job_elem in job_elems:
             date_elem = job_elem.find('span', class_='date')
             date = date_elem.text.strip()
             dates.append(date)
-        extracted_info.append(dates)
-        
-        for j in range(len(cols)-1):
-            jobs_list[cols[j]] = extracted_info[j]
-        
-        num_listings = len(extracted_info[0])
+ 
         page_num += 1
         
-    jobs = pd.DataFrame(jobs_list)
-    jobs.to_excel(f"{job_title} jobs in {location}.xlsx")
+    jobs = pd.DataFrame({'Titles' : titles , 'companies' : companies , 'links' : links , 'date_listed' : dates})
+    jobs.to_csv(f"{job_title} jobs in {location}.csv")
   
 
 if __name__ == '__main__':
